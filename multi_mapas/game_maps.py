568e8378967d92fbbaf770ff11413130d5324a55
Inteligência Mil Grau <48864772+inteligenciamilgrau@users.noreply.github.com>
2025-07-19 02:19:54 -0300

    Create game_maps.py

---
+++ multi_mapas/game_maps.py
@@ -0,0 +1,68 @@
+MAPS = [
+    {
+        "name": "Simples",
+        "layout": [
+            "###########",
+            "#OPOOOOOOR#",
+            "#OOOOOOOOO#",
+            "#OOOOOOOOO#",
+            "#OOOOOOOOO#",
+            "#OOOOOOOOO#",
+            "#OOOOOOOOO#",
+            "#OOOOOOOOO#",
+            "#OOOOOOOOO#",
+            "#OOOOOOOOO#",
+            "###########",
+        ]
+    },
+    {
+        "name": "Sorriso",
+        "layout": [
+            "###########",
+            "#OOOOOOOOO#",
+            "#OOOOOOOOO#",
+            "#OOOPOOOOO#",
+            "#OOOOOOOOO#",
+            "#OO#OOO#OO#",
+            "#OOO###OOO#",
+            "#OOOOOROOO#",
+            "#OOOOOOOOO#",
+            "#OOOOOOOOO#",
+            "###########",
+        ]
+    },
+    {
+        "name": "Labirinto",
+        "layout": [
+            "###########",
+            "#POOOOOOOO#",
+            "#OOOOOOOOO#",
+            "#OO#OOO#OO#",
+            "#OO#OOO#OO#",
+            "####OOO#OO#",
+            "#OOOOOO#OO#",
+            "#OOOOOO#OO#",
+            "########OO#",
+            "#OOOOOOOOO#",
+            "#ROOOOOOOO#",
+            "###########",
+        ]
+    },
+    {
+        "name": "Barreira",
+        "layout": [
+            "#############",
+            "#OOOOOOOOOOO#",
+            "#OOOOOOOOOOO#",
+            "#OO##OOO##OO#",
+            "#OO#OOOOO#OO#",
+            "#OO#OOOOO#OO#",
+            "#OO#OOPOO#OO#",
+            "#OO#OOOOO#OO#",
+            "#OO#######OO#",
+            "#OOOOOOOOOOO#",
+            "#OOOOOROOOOO#",
+            "#############",
+        ]
+    },
+]
